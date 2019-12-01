61677cc082637cc82b6e0af526bac31bd60c3f84
ninjapretzel <ninjapretzel@yahoo.com>
2019-11-30 22:43:28 -0700

    Adds inlining suggestions for JsonDeserializer.index

--- XtoJSON.cs
+++ XtoJSON.cs
@@ -58,6 +58,7 @@ using System.Globalization;
 using System.Reflection;
 using System.Linq;
 using System.Text; // Needed when paired alongside ZSharp, since StringBuilder is wrapped (outside of namespace)
+using System.Runtime.CompilerServices;
 
 #region Abstract/Primary stuff
 
@@ -2555,9 +2556,12 @@ public class JsonDeserializer {
 	private string json;
 	/// <summary> Internal index. Do not modify outside of <see cref="index"/>.set </summary>
 	private int __index;
+	
 	/// <summary> Current position. </summary>
 	private int index {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		get { return __index; }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		set {
 			if (value < __index) {
 				throw new JsonDeserializeFailedException("JsonDeserializer.index.set: Cannot set index below current value", this);
