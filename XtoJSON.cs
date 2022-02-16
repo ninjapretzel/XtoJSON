0256c4dd37a871b7e7c967cac440b61c138ecf88
ninjapretzel <ninjapretzel@yahoo.com>
2022-02-16 08:43:29 -0700

    Changes to copyleft

--- XtoJSON.cs
+++ XtoJSON.cs
@@ -1,9 +1,9 @@
 ﻿/*	XtoJSON
 	Lightweight JSON Library for C#
-	2015-2020  Jonathan Cohen
+	2015-2022  Jonathan Cohen
 	Contact: ninjapretzel@yahoo.com
 
-	Copyright (c) 2020 Jonathan Cohen aka ninjapretzel
+	Copyleft (c) 2022 Jonathan Cohen aka ninjapretzel
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -12,13 +12,13 @@
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in all
+	The above copyleft notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
 
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	AUTHORS OR COPYLEFT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
